5c84630007f74e8a3be1d463aee9cc0a9b9bd2e1
pona8257 <pona8257@naver.com>
2023-03-27 19:49:09 +0900

    2023-03-27

---
+++ login.js
@@ -0,0 +1,34 @@
+
+
+        var idPass = false;
+
+        $('#id').on('keyup', function(){
+            // 아이디 입력란에 입력된 값 : $(this).val() == this.value  자바 스크립트
+            let idValue = $(this).val();
+            if(idValue.length == 0){
+                $('#msg_id').text('');  // 지운다
+                idPass = false;
+            } else if(idValue.length < 4){
+                $('#msg_id').text('아이디는 4자 이상입니다.').css('color', '#FF0000').css('font-size', '14px');
+                idPass = false;
+            } else {
+                $('#msg_id').text('정상적인 아이디입니다.').css('color', '#0000FF').css('font-size', '14px');
+                idPass = true;
+            }
+            // idPass = idValue.length >= 4;    하나씩 쓰거나 이것처럼 한번에 쓰거나
+        })
+
+        $('#frm_login').on('submit', function(event){
+            if(idPass == false){
+                alert('아이디를 확인하세요.');
+                event.preventDefault();
+                return;
+            }
+            if($('#pw').val() == ''){
+                alert('비밀번호를 입력하세요.');
+                event.preventDefault();
+                return;
+            }
+        })
+
+    
